db4ae486f4ea1d7daeedd17aa924064c6e52194a
EgorVereshko <72032005+EgorVereshko@users.noreply.github.com>
2023-09-25 14:28:09 +0500

    Update seven.py

--- seven.py
+++ seven.py
@@ -1,17 +1,17 @@
-def pyramid(n):
+def pyramid(number):
     result = 0
     k = 0
     for i in range(1, 100):
         result += i**2
         k += 1
-        if result == n:
+        if result == number:
             return k
         continue
-    if result != n:
+    if result != number:
         return "It is impossible"
 
 
 print(pyramid(5))
 print(pyramid(14))
 print(pyramid(30))
-print(pyramid(1250))
+print(pyramid(1000000))
