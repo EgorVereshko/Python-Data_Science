08251bf7e79a1f8fab42b5b34aaf0b0caf28bb2d
EgorVereshko <72032005+EgorVereshko@users.noreply.github.com>
2023-09-25 14:27:36 +0500

    Update five.py

--- five.py
+++ five.py
@@ -1,9 +1,9 @@
-def func(n):
-    summa = 0
-    for i in range(1, n+1):
-        summa += i
-    sr = summa / n
-    result = 1/n * summa * (n-sr)**2
+def func(vector):
+    total = 0
+    for i in range(1, vector+1):
+        total += i
+    average = total / vector
+    result = 1/vector * total * (vector-average)**2
     return result
 
 
