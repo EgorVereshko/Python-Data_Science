09e60467caeea233410218d597c37d55fea45b3d
EgorVereshko <72032005+EgorVereshko@users.noreply.github.com>
2023-10-23 09:15:19 +0500

    Update numpy_7.py

--- Lab1_numpy/numpy_7.py
+++ Lab1_numpy/numpy_7.py
@@ -2,14 +2,14 @@ import numpy as np
 
 
 def time_line_analysis(line, window_size):
-    mean = np.mean(line) # мат ожидание
-    var = np.var(line) # дисперсия
-    std = np.std(line) # Вычислите стандартное отклонение вдоль указанной оси.
+    mean = np.mean(line)
+    var = np.var(line)
+    std = np.std(line)
 
     local_max = []
     local_min = []
 
-    for i in range(1, len(line)-1): # перебор элементов ряда
+    for i in range(1, len(line)-1):
         if line[i] > line[i-1] and line[i] > line[i+1]:
             local_max.append(line[i])
         elif line[i] < line[i-1] and line[i] < line[i+1]:
@@ -20,10 +20,8 @@ def time_line_analysis(line, window_size):
     return mean, var, std, local_max, local_min, moving_average
 
 
-# Пример временного ряда
 line = np.array([1, 2, 3, 2, 1, 5, 6, 4, 2, 3])
 
-# Вызов функции для анализа ряда
 mean, var, std, local_max, local_min, moving_average = time_line_analysis(line, window_size=3)
 
 print("Математическое ожидание:", mean)
